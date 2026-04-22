38c78a92846ae454ac76c3ffd083c7127d37e3de
solom <222359366@mycput.ac.za>
2026-04-23 00:49:04 +0200

    ServiceItemRepository

--- src/main/java/za/ac/cput/unitrade/repository/ServiceItemRepository.java
+++ src/main/java/za/ac/cput/unitrade/repository/ServiceItemRepository.java
@@ -1,16 +1,68 @@
 package za.ac.cput.unitrade.repository;
 
 import za.ac.cput.unitrade.dto.ServiceItemDTO;
+
+import java.util.ArrayList;
 import java.util.List;
 
+public class ServiceItemRepository implements IServiceItemRepository {
 
-public interface ServiceItemRepository {
+    private static IServiceItemRepository repository = null;
+    private final List<ServiceItemDTO> itemList;
 
-    void add(ServiceItemDTO item);
+    private ServiceItemRepository() {
+        itemList = new ArrayList<>();
+    }
 
-    List<ServiceItemDTO> getAll();
+    // singleton access
+    public static IServiceItemRepository getRepository() {
+        if (repository == null) {
+            repository = new ServiceItemRepository();
+        }
+        return repository;
+    }
 
-    ServiceItemDTO getById(int id);
+    @Override
+    public ServiceItemDTO create(ServiceItemDTO item) {
+        itemList.add(item);
+        return item;
+    }
 
-    void delete(int id);
+    @Override
+    public ServiceItemDTO read(Integer id) {
+        for (ServiceItemDTO item : itemList) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public ServiceItemDTO update(ServiceItemDTO item) {
+        ServiceItemDTO existing = read(item.getId());
+
+        if (existing != null) {
+            itemList.remove(existing);
+            itemList.add(item);
+            return item;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        ServiceItemDTO item = read(id);
+
+        if (item != null) {
+            itemList.remove(item);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<ServiceItemDTO> getAll() {
+        return new ArrayList<>(itemList);
+    }
 }
